6853e671515a3dd55188f40b6040673362a3bda5
w1635610 <1635610@163.com>
2021-02-23 02:59:44 -0800

    update script

--- 6_Tools/install_toolchain.sh
+++ 6_Tools/install_toolchain.sh
@@ -1,6 +1,5 @@
 sudo rm -rf /usr/src/aarch64-linux-gnu
-sudo tar -xvf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz -C /usr/src/
-sudo mv /usr/src/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu /usr/src/aarch64-linux-gnu
+sudo cp -rf ./aarch64-linux-gnu /usr/src/
 
 #echo "export ARCH_HOME=/usr/src/aarch64-linux-gnu" >> ~/.bashrc
 #echo "export PATH=\$PATH:\$ARCH_HOME/bin" >> ~/.bashrc
