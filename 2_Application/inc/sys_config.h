69ae50c975b5facc5e738fd4309d90792a47f643
w1635610 <1635610@163.com>
2021-03-05 16:31:52 +0000

    updata json config file and interface

--- 2_Application/inc/sys_config.h
+++ 2_Application/inc/sys_config.h
@@ -17,8 +17,22 @@
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
+/* Exported value ------------------------------------------------------------*/
+extern json_object *config_json_obj;
+
 /* Exported functions --------------------------------------------------------*/ 
-extern json_object *pobj;
-extern void config_init(void);
+void config_init(void);
+
+/*single*/
+const char *config_get_string(json_object *obj_base, char *key,const char* defval);
+int config_get_int(json_object *obj_base, char *key, int defval);
+int config_get_bool(json_object *obj_base, char *key, int defval);
+
+/*array*/
+int config_get_array_lenth(json_object *obj_base, char *key);
+json_object *config_get_array_obj(json_object *obj_base, char *key, int index);
+const char *config_get_array_string(json_object *obj_base, char *key, int index,const char* defval);
+int config_get_array_int(json_object *obj_base, char *key, int index, int defval);
+int config_get_array_bool(json_object *obj_base, char *key, int index, int defval);
 
 #endif
