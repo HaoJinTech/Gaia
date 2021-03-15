7d6ace380b00a824ad5821f8dd8caae9393fe741
w1635610 <1635610@163.com>
2021-03-15 10:22:28 +0000

    fix script issue

--- 2_Application/01_copy_release.sh
+++ 2_Application/01_copy_release.sh
@@ -1,6 +1,6 @@
-sudo rm -rf /etc/Gaia
-sudo mkdir /etc/Gaia
+sudo rm -rf /usr/Gaia
+sudo mkdir /usr/Gaia
 
-sudo cp ./Gaia /etc/Gaia/
-sudo cp -rf ./prj_files /etc/Gaia
+sudo cp ./Gaia /usr/Gaia/
+sudo cp -rf ./prj_files /usr/Gaia
 sudo cp ./rc.local /etc/
