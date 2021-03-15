fb213bdbfe7601cab23a01433972dca79e02c40e
w1635610 <1635610@163.com>
2021-03-15 20:17:51 +0000

    add spi_ex, a other spi bus

--- 2_Application/src/combo_bus.c
+++ 2_Application/src/combo_bus.c
@@ -12,7 +12,8 @@
 /* Includes ------------------------------------------------------------------*/
 #include "platform.h"
 #include "bus/bus_dump.c"
-#include "bus/bus_spi.c"
+//#include "bus/bus_spi.c"
+#include "bus/bus_spi_ex.c"
 #include "bus/bus_uart.c"
 // TODO: include new bus src here
 
